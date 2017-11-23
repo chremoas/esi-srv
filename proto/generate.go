ec403efd0340bbcb8297b7327a88390ebfeb9a63
Matt Maurer <maurer.it@gmail.com>
2017-11-23 15:38:45 -0500

    Mocks and another generator

--- proto/generate.go
+++ proto/generate.go
@@ -1,3 +1,4 @@
 package chremoas_esi
 
 //go:generate protoc --go_out=plugins=micro:. esi-srv.proto
+//go:generate mockgen -package=chremoas_esi -source=esi-srv.pb.go -destination=proto_mocks.go EntityQueryClient
