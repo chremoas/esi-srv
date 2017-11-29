300bc6db0858368be3913f5870173ec7990f1b5e
Brian Hechinger <wonko@4amlunch.net>
2017-11-29 12:56:41 -0500

    Better layout hand crafted Protobuf

--- proto/generate.go
+++ proto/generate.go
@@ -1,3 +1,6 @@
 package chremoas_esi
 
-//go:generate protoc --go_out=plugins=micro:. esi-srv.proto
+//go:generate protoc --go_out=plugins=micro:. alliance.proto
+//go:generate protoc --go_out=plugins=micro:. character.proto
+//go:generate protoc --go_out=plugins=micro:. corporation.proto
+//go:generate protoc --go_out=plugins=micro:. search.proto
