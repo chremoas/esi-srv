626971d7d6b669a428ff2cfe6a1f13bec991b344
Brian Hechinger <wonko@4amlunch.net>
2017-12-05 13:53:00 -0500

    now with a mock-esi container

---
+++ handler/Alliance_test.go
@@ -0,0 +1,20 @@
+package handler
+
+import (
+	"testing"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestSpec(t *testing.T) {
+	Convey("Given some integer with a starting value", t, func() {
+		x := 1
+
+		Convey("When the integer is incremented", func() {
+			x++
+
+			Convey("The value should be greater by one", func() {
+				So(x, ShouldEqual, 2)
+			})
+		})
+	})
+}
