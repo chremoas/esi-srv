1c866b5d6caf082d53e4025a45c874fbe7a11e8e
Brian Hechinger <wonko@4amlunch.net>
2021-03-17 16:16:21 -0400

    So many things to check in that I've ignored for so long. I'm a terrible person.

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.14
 
 require (
 	github.com/antihax/goesi v0.0.0-20190723215635-487b927dc566
-	github.com/chremoas/services-common v1.3.1
+	github.com/chremoas/services-common v1.3.2
 	github.com/golang/mock v1.3.1
 	github.com/golang/protobuf v1.3.2
 	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79
@@ -12,8 +12,10 @@ require (
 	github.com/micro/go-micro v1.9.1
 	github.com/petergtz/pegomock v2.8.0+incompatible
 	github.com/smartystreets/goconvey v0.0.0-20190710185942-9d28bd7c0945
+	go.uber.org/zap v1.10.0
 	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80
 )
 
 replace github.com/chremoas/esi-srv => ../esi-srv
+
 replace github.com/hashicorp/consul => github.com/hashicorp/consul v1.5.1
--- main.go
+++ main.go
@@ -2,15 +2,35 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/chremoas/services-common/config"
-	"github.com/chremoas/esi-srv/proto"
+	"go.uber.org/zap"
+
+	chremoasPrometheus "github.com/chremoas/services-common/prometheus"
+
 	"github.com/chremoas/esi-srv/handler"
+	"github.com/chremoas/esi-srv/proto"
 )
 
-var Version = "SET ME YOU KNOB"
-var name = "esi"
+var (
+	Version = "SET ME YOU KNOB"
+	name    = "esi"
+	logger  *zap.Logger
+)
 
 func main() {
+	var err error
+
+	// TODO pick stuff up from the config
+	logger, err = zap.NewProduction()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+	logger.Info("Initialized logger")
+
+	go chremoasPrometheus.PrometheusExporter(logger)
+
 	service := config.NewService(Version, "srv", name, config.NilInit)
 
 	chremoas_esi.RegisterAllianceServiceHandler(service.Server(), handler.NewAllianceServiceHandler())
